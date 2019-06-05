8b6523c08b37b4d621179500b5efd0226074c800
mateuszrosa <mateusz.rosa@gmail.com>
2019-06-05 16:15:50 +0200

    rebuild app

--- js/AddingTasker.js
+++ js/AddingTasker.js
@@ -10,21 +10,5 @@ class AddingTasker {
             alert('Write task text into input!');
         }
     }
-    removeTaskFromList() {
-        console.log('click');
-    }
-    addTaskToList(text, ul) {
-        console.log(this.num);
-        this.num++;
-        const newLi = document.createElement('li');
-        newLi.innerHTML = `${text} <span class='close'>x</span>`;
-        newLi.style.backgroundColor = "#ccc"
-        if (this.num % 2 === 0) newLi.style.backgroundColor = "#f9f9f9";
-        ul.append(newLi);
-        document.querySelectorAll('span.close').forEach(span => {
-            span.addEventListener('click', RemovingTasker.removeTaskFromList)
-        })
-        newLi.addEventListener('click', TickingTasker.tickTask);
-        newLi.addEventListener('doubleclick', TickingTasker.edit);
-    }
+    addTaskToList() {}
 }
--- js/main.js
+++ js/main.js
@@ -3,13 +3,22 @@ class toDoList {
         this.input = document.querySelector('input');
         this.ul = document.querySelector('ul');
         this.addTasker = new AddingTasker;
+        this.num = 0;
         document.querySelector('span').addEventListener('click', this.startApp.bind(this));
     }
-    render() {}
+    render() {
+        const newLi = document.createElement('li');
+        this.addTasker.tasks.forEach((task) => {
+            newLi.textContent = task;
+        })
+        this.ul.append(newLi);
+        this.num++;
+    }
     startApp() {
+        console.log(this.addTasker.tasks);
         const task = this.input.value;
-        this.addTasker.addTaskToList(task, this.ul);
-        this.input.value = "";
+        this.addTasker.addTask(this.input.value);
+        this.render();
     }
 }
 
