67d8dfd76e28f945d32802ee9df82707f046b7d5
mateuszrosa <mateusz.rosa@gmail.com>
2019-06-05 19:24:18 +0200

    rebuilding app

--- js/main.js
+++ js/main.js
@@ -3,16 +3,15 @@ class toDoList {
         this.input = document.querySelector('input');
         this.ul = document.querySelector('ul');
         this.addTasker = new AddingTasker;
-        this.num = 0;
         document.querySelector('span').addEventListener('click', this.startApp.bind(this));
     }
     render() {
         const newLi = document.createElement('li');
         this.addTasker.tasks.forEach((task) => {
-            newLi.textContent = task;
+            newLi.innerHTML = `${task} <span class='close'>x</span>`;
         })
         this.ul.append(newLi);
-        this.num++;
+        this.input.value = "";
     }
     startApp() {
         console.log(this.addTasker.tasks);
