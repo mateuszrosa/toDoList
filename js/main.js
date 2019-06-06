bfb171a447136442f42658cae93093b8c1e3587f
mateuszrosa <mateusz.rosa@gmail.com>
2019-06-06 17:57:30 +0200

    removing task from list and from array

--- js/main.js
+++ js/main.js
@@ -7,26 +7,36 @@ class ToDoList {
         this.span.addEventListener('click', this.startApp.bind(this));
         this.tasks = [];
     }
-    render(arr) {
+    render() {
         const newLi = document.createElement('li');
         this.tasks.forEach((task, index) => {
             newLi.innerHTML = `${task} <span class='close'>x</span>`;
             newLi.dataset.key = index;
         });
+        newLi.querySelector('span.close').addEventListener('click', e => {
+            this.remove(e);
+        });
         this.ul.append(newLi);
 
     }
     add(value) {
+        console.log(this.tasks);
         this.tasks.push(value);
         return this.tasks;
     }
-    remove() {
-        console.log(this);
+    remove(e) {
+        const index = e.target.parentNode.dataset.key;
+        console.log(index);
+        console.log(this.tasks[index]);
+        this.tasks.splice(index, 1);
+        console.log(this.tasks);
+        e.target.parentNode.remove();
     }
     startApp() {
         if (this.input.value === "") return alert("Write task!");
         this.tasks = this.add(this.input.value);
         this.render();
+        this.input.value = "";
     }
 }
 
