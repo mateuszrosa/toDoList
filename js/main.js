b59230fa3e85fed36960dbc4df4a8521fa44b9bc
mateuszrosa <mateusz.rosa@gmail.com>
2019-06-06 16:11:21 +0200

    rebuilding app

--- js/main.js
+++ js/main.js
@@ -1,19 +1,33 @@
-class toDoList {
+class ToDoList {
     constructor() {
         this.input = document.querySelector('input');
         this.span = document.querySelector('span.add');
         this.taskNumber = document.querySelector('span.taskNumber');
+        this.ul = document.querySelector('ul');
         this.span.addEventListener('click', this.startApp.bind(this));
         this.tasks = [];
+    }
+    render(arr) {
+        const newLi = document.createElement('li');
+        this.tasks.forEach((task, index) => {
+            newLi.innerHTML = `${task} <span class='close'>x</span>`;
+            newLi.dataset.key = index;
+        });
+        this.ul.append(newLi);
+
     }
     add(value) {
         this.tasks.push(value);
         return this.tasks;
     }
+    remove() {
+        console.log(this);
+    }
     startApp() {
-        if (this.input.value === "") return alert("Wtire task!");
-        this.add(this.input.value);
+        if (this.input.value === "") return alert("Write task!");
+        this.tasks = this.add(this.input.value);
+        this.render();
     }
 }
 
-const todolist = new toDoList;
+const app = new ToDoList;
