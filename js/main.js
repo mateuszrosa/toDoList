bde277485294ba57512e6fd2c18eafd3c1c03ca5
mateuszrosa <mateusz.rosa@gmail.com>
2019-06-07 11:02:24 +0200

    ticking tasks function

--- js/main.js
+++ js/main.js
@@ -5,17 +5,10 @@ class ToDoList {
         this.taskNumber = document.querySelector('span.taskNumber');
         this.ul = document.querySelector('ul');
         this.span.addEventListener('click', this.startApp.bind(this));
-        this.tasks = [];
+        this.tasks = document.getElementsByClassName('task');
         this.taskNumber.textContent = this.tasks.length;
     }
     render() {
-
-        // newLi.querySelector('span.close').addEventListener('click', e => {
-        //     this.remove(e);
-        // });
-        // newLi.addEventListener('click', e => {
-        //     this.done(e);
-        // });
         this.ul.append(this.add(this.input.value));
         this.taskNumber.textContent = this.tasks.length;
     }
@@ -23,18 +16,20 @@ class ToDoList {
         const newLi = document.createElement('li');
         newLi.classList.add('task');
         newLi.innerHTML = `${value} <span class='close'>x</span>`;
-        newLi.querySelector('span.close').addEventListener('click', this.remove);
+        newLi.querySelector('span.close').addEventListener('click', this.remove.bind(this));
+        newLi.addEventListener('click', this.done);
         return newLi;
     }
     remove(e) {
-        this.parentNode.remove();
+        e.target.parentNode.remove();
+        this.taskNumber.textContent = this.tasks.length;
     }
     done(e) {
         e.target.classList.add('checked');
     }
     startApp() {
         if (this.input.value === "") return alert("Write task!");
-        this.tasks = this.add(this.input.value);
+        this.add(this.input.value);
         this.render();
         this.input.value = "";
     }
